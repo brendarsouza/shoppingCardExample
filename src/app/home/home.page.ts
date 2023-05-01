ab429f69816d0613b2c7db70c902ccebdbc38afc
Brenda Souza <si.brendarenata@gmail.com>
2023-05-01 17:16:40 -0300

    feat: init layout

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CartService, Product } from '../services/cart.service';
+import { ModalController } from '@ionic/angular';
+import { BehaviorSubject } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  cart = [];
+  products = [] as Product[];
+  cartItemCount: BehaviorSubject<number> = {} as BehaviorSubject<number>;
 
-  constructor() {}
+  constructor(private cartService: CartService, private modalCtrl: ModalController) {}
 
+  ngOnInit(): void {
+    this.products = this.cartService.getProducts();
+    this.cart = this.cartService.getCart();
+    this.cartItemCount = this.cartService.getCartItemCount();
+  }
+
+  addToCart(product) {
+
+  }
+
+  openCart() {
+
+  }
 }
